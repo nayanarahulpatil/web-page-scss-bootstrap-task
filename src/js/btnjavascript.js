f169859351ab83bd7830ca99ab6cf462a60bebf4
unknown <nayana.shinde04@gmail.com>
2021-09-16 11:26:51 +0530

    changes done which mam told

---
+++ src/js/btnjavascript.js
@@ -0,0 +1,17 @@
+function btn_knowMore(){
+    alert("hi");
+    console.log("hllo");
+}
+
+function btn_viewall(){
+    alert("hi");
+    console.log("hllo");
+}
+function btn_knowmore_servises(){
+    alert("hi");
+    console.log("hwlo");
+  }
+function btn_subscribe(){
+    alert("hi");
+    console.log("hllo");
+}
